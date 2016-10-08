0820f757849c6adf299b1eb9ad7b533af1615efa
mjdude <mo@syncspot.net>
2016-10-07 22:22:44 -0400

    initialised npm

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const notes = require('./notes.js');
 
 var res = notes.addNote();
 var res2 = notes.add( 1, 2);
-console.log(res);
 
-console.log(res2);
+console.log('Add 9 plus 3 is ', notes.add(9, 3));
 // var user = os.userInfo();
 
 // fs.appendFile('greetings.txt', `Hello ${user.username} !`);
