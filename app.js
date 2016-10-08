7c14a1d7b3ba2c0cdde4a2279f4641bc86340a35
mjdude <mo@syncspot.net>
2016-10-07 22:40:58 -0400

    clean up project

--- app.js
+++ app.js
@@ -1,18 +1,6 @@
 console.log('Starting app.js ..');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js');
-
-var filteredArray = _.uniq(['Mo', 1, 'Mo', 1 ,2,3,4 ]);
-console.log(filteredArray);
-
-// console.log(_.isString(true));
-//
-// console.log(_.isString('window'));
-
-// console.log('Add 9 plus 3 is ', notes.add(9, 3));
-// var user = os.userInfo();
-
-// fs.appendFile('greetings.txt', `Hello ${user.username} !`);
